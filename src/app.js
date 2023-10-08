d0cd75d985e27f8d4a7eb2ce0b2703d141f8296d
CrisMoreiras <137789637+CrisMoreiras@users.noreply.github.com>
2023-10-08 01:14:59 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,44 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let excuseGenerator = () => {
+    let who = ["Mi perro", "Mi gato", "Mi hermana", "Mi jefa", "Mi suegra"];
+    let action = ["se comió", "se llevó", "escondió", "regaló", "rompió"];
+    let what = [
+      "mi tarea",
+      "mi coche",
+      "mi movil",
+      "mi ordenador",
+      "mis gafas"
+    ];
+    let when = [
+      "ayer",
+      "hace un mes",
+      "la semana pasada",
+      "el año pasado",
+      "la noche pasada"
+    ];
+
+    let whoRandom = getObjectrandom(who);
+    let actionRandom = getObjectrandom(action);
+    let whatRandom = getObjectrandom(what);
+    let whenRandom = getObjectrandom(when);
+
+    return (
+      who[whoRandom] +
+      " " +
+      action[actionRandom] +
+      " " +
+      what[whatRandom] +
+      " " +
+      when[whenRandom]
+    );
+  };
+
+  function getObjectrandom(myArray) {
+    return Math.floor(Math.random() * myArray.length);
+  }
+
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
 };
